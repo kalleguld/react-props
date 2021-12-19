05482a02fae7f4183497afafbe40a95fc0699582
Kasper Guldmann Nielsen <git@kalleguld.dk>
2021-12-19 04:45:37 +0100

    export new feature

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export type { Prop } from "./prop";
 export { Checkbox } from "./Checkbox";
 export { Input } from "./Input";
 export { useProp } from "./useProp";
+export { usePersistentProp } from './usePersistentProp'
 export { MapProp } from "./MapProp";
 export { map } from './map';
 export { NumInput } from './NumInput';
