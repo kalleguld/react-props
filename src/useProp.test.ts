2375bf213035631160f2a9bf3403396d40681fd8
Kasper Guldmann Nielsen <git@kalleguld.dk>
2021-12-21 18:53:53 +0100

    useProp: fix wrong interface

--- src/useProp.test.ts
+++ src/useProp.test.ts
@@ -1,5 +1,5 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import { useProp} from './useProp';
+import { useProp } from './useProp';
 
 test('initial value can be set correctly', () => {
     const { result } = renderHook(() => {
--- src/useProp.ts
+++ src/useProp.ts
@@ -3,7 +3,7 @@ import { Prop } from "./prop";
 
 /**Like {@link useState}, except it returns a {@link Prop Prop&lt;T&gt;} */
 export function useProp<T>(): Prop<T|undefined>
-export function useProp<T>(initialValueFunc: ( (t: T)=> T)): Prop<T>;
+export function useProp<T>(initialValueFunc: ( ()=> T)): Prop<T>;
 export function useProp<T>(initialValue: T): Prop<T>;
 export function useProp<T>(initialValue?: T | ( () => T)): Prop<T|undefined>{
     
