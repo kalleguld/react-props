4604828cc18086112a0fca0876455479deb65e8d
Kasper Guldmann Nielsen <git@kalleguld.dk>
2021-12-21 21:43:25 +0100

    better test coverage

--- src/BasicProp.test.ts
+++ src/BasicProp.test.ts
@@ -11,6 +11,17 @@ test("setting a new value works", () => {
     expect(dut.value).toEqual(4);
 });
 
+test("setting a new value using a function works", () => {
+    const dut = new BasicProp(1);
+    let oldReported: any = null;
+    dut.set(r =>{
+        oldReported = r;
+        return 4;
+    });
+    expect(dut.value).toEqual(4);
+    expect(oldReported).toEqual(1);
+});
+
 test("setting a value multiple times emits the last result", ()=> {
     const dut = new BasicProp(1);
     
